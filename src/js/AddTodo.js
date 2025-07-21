21f6e2af69530d186911efa932611089f2ccafd9
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-07-21 16:07:01 +0330

    addTodo is not reting

--- src/js/AddTodo.js
+++ src/js/AddTodo.js
@@ -1,7 +1,38 @@
 const addTodo = document.querySelector('#addTodo');
 const todoModel = document.querySelector('#todoModel');
 const cancelBtn = document.querySelector('#cancel_model_addTodo')
+const todoTitle  = document.querySelector("#title_todo")
+const todoSubject  = document.querySelector("#subject_todo")
+const saveTodo = document.querySelector('#addTodo_save');
+const errorElem = document.querySelector("#error");
 
+function handlerSaveTodo (){
+    const titleValue = todoTitle.value;
+    const subjectValue = todoSubject.value;
+    if(!titleValue.trim() || !subjectValue.trim()){
+        alert("All filed required");
+        return
+    }
+    if(titleValue.length > 25 ){
+        alert("Title must be at least 25 characters")
+        return
+    }
+    const todo = {
+        id:crypto.randomUUID(),
+        title: titleValue,
+        subject: subjectValue,
+        date:Date.now()
+    }
+
+    let todos = JSON.parse(localStorage.getItem("todos"))|| [];
+
+    todos.push(todo)
+    localStorage.setItem("todos",JSON.stringify(todos));
+    todoTitle.value ="";
+    todoSubject.value = "";
+    alert("Todo Add Successfully ✅");
+    todoModel.classList.remove('active')
+}
 
 
  function handlerAddTodo (){
@@ -16,9 +47,11 @@ function handlerExitModel (){
 }
 
 export function  addTodos (){
-    handlerAddTodo
+    handlerAddTodo;
+    handlerSaveTodo;
+}   
 
-}
 
 addTodo.addEventListener("click" ,handlerAddTodo)
 cancelBtn.addEventListener("click" , handlerExitModel)
+saveTodo.addEventListener('click', handlerSaveTodo)
